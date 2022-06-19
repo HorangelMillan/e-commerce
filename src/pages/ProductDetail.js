50f942d1bdd9d28f6f20124e9c0e7be6882fc7a9
Horangel Millan <horangelmillan@outlook.com>
2022-06-18 22:39:28 -0500

    added galery with animations and page icon

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -13,6 +13,7 @@ const ProductDetail = () => {
 
     const [product, setProduct] = useState({});
     const [quantity, setQuantity] = useState(0);
+    const [translate, setTranslate] = useState(0);
 
     const dispatch = useDispatch();
     const products = useSelector(state => state.products);
@@ -32,17 +33,37 @@ const ProductDetail = () => {
         setQuantity(0);
     }
 
+    const translateImg = (mod, limit) => {
+
+            if (mod === 'next' && translate <= limit - 2) {
+                setTranslate(translate + 1);
+            } else if (mod === 'previous' && translate > 0) {
+                setTranslate(translate - 1);
+            };
+
+    };
+
+    console.log(translate)
+
     return (
         <div className='product-detail'>
             <p>Home  <span>•</span>  <b>{product.title}</b></p>
             <div className='product'>
                 <div className='product-image'>
                     <div className='preview'>
-                        {
-                            product.productImgs && (
-                                <img src={product.productImgs[0]} alt="" />
-                            )
-                        }
+                        <button className='previous' onClick={() => translateImg('previous', product.productImgs.length)}><i className="fas fa-chevron-left"></i></button>
+                        <ul>
+                            {
+                                product.productImgs && product.productImgs.map(productImg => (
+
+                                    <li key={productImg} style={{ transform: `translateX(-${translate}00%)` }}>
+                                        <img src={productImg} alt="" />
+                                    </li>
+
+                                ))
+                            }
+                        </ul>
+                        <button className='next' onClick={() => translateImg('next', product.productImgs.length)}><i className="fas fa-chevron-right"></i></button>
                     </div>
                     <div className='galery'>
                         {
@@ -57,7 +78,7 @@ const ProductDetail = () => {
 
                 <div className='product-shop'>
 
-                    <input type="number" onChange={e => setQuantity(e.target.value)} value={quantity}/>
+                    <input type="number" onChange={e => setQuantity(e.target.value)} value={quantity} />
                     <button onClick={() => addProductFromShop()}>Add to cart</button>
 
                 </div>
